1b4095888a5193357b55bd47b04c33212eab79c2
Enzo Degraeve <enzodeg40@gmail.com>
2023-07-16 16:56:54 +0200

    Ajout de panorama

--- js/data.js
+++ js/data.js
@@ -17,9 +17,79 @@ const data = [
   },
   {
     id: 3,
+    "name": "MLC la citrouille",
     gps: "48.564772, 2.608060",
     mapsid: "XbdCFkrfWI7k4Cud7MnloA",
     n: 194,
     url: "https://www.google.fr/maps/@48.5647723,2.6080599,3a,60y,90t/data=!3m6!1e1!3m4!1sXbdCFkrfWI7k4Cud7MnloA!2e0!7i16384!8i8192?entry=ttu"
+  },
+  {
+    id: 4,
+    name: "Rue du Moulin À Vent",
+    gps: "48.571300, 2.598372",
+    mapsid: "GrUZUd5v7JHwL9h12UShjg",
+    n: 70,
+    url: "https://www.google.fr/maps/@48.5713001,2.5983722,3a,60y,273.48h,83.32t/data=!3m6!1e1!3m4!1sGrUZUd5v7JHwL9h12UShjg!2e0!7i16384!8i8192?entry=ttu"
+  },
+  {
+    id: 5,
+    name: "Médiathèque",
+    gps: "48.565492, 2.603940",
+    mapsid: "PO_xdK3a_WiNRFZOLFEZ4w",
+    n: 96,
+    url: "https://www.google.fr/maps/@48.5654923,2.6039402,3a,60y,352.54h,73.26t/data=!3m6!1e1!3m4!1sPO_xdK3a_WiNRFZOLFEZ4w!2e0!7i13312!8i6656?entry=ttu"
+  },
+  {
+    id: 6,
+    gps: "48.571903, 2.603914",
+    mapsid: "5WX7PsFzA0T-9jp2ncG0Ag",
+    n: 79,
+  },
+  {
+    id: 7,
+    gps: "48.567267, 2.600485",
+    mapsid: "g-hn6DLyfj-nEMB2tesk-w",
+    n: 70,
+  },
+  {
+    id: 8,
+    gps: "48.569356, 2.595525",
+    mapsid: "tBDmeFmGRSnsH86MPYuXnA",
+    n: 211,
+  },
+  {
+    id: 9,
+    gps: "48.565554, 2.603300",
+    mapsid: "hG8lCU-q9OOSmvGm3f-QoA",
+    n: 4,
+  },
+  {
+    id: 10,
+    gps: "48.563615, 2.607685",
+    mapsid: "25KPDSpE00aj4RtZXSGurg",
+    n: 310,
+  },
+  {
+    id: 11,
+    gps: "48.563441, 2.601213",
+    mapsid: "IxSrDcq5wUA_ZkwlyEAXhA",
+    n: 56,
   }
+
 ]
+
+/*
+
+
+  9
+  48.565554, 2.603300
+  hG8lCU-q9OOSmvGm3f-QoA
+  4
+
+  10 
+  48.563615, 2.607685
+  25KPDSpE00aj4RtZXSGurg
+  310
+
+
+*/
